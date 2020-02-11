37dec83bbd16e79b580023bd339b0e712a0d3f2f
Archana Karki <39858235+archanakarki@users.noreply.github.com>
2020-02-11 17:28:37 +0200

    Implementation of Read functionality, Model created

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
-# Specifying __name__ inside Flask creates an app that gets named after the name of this file which is app
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://archanakarki@localhost:5432/todoapp'
+
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+# Debugging statement
+def __repr__(self):
+    return f'<Todo {self.id} {self.description}>'
+
+db.create_all()
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=[{
-        'description': 'Todo 1'
-    },{
-        'description': 'Todo 2'
-    },{
-        'description': 'Todo 3'
-    }])
+    return render_template('index.html', data=Todo.query.all())
