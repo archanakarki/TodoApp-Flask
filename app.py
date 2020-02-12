06772ad4f8d621c1ed8981af622f3a7fee9a7cfa
Archana Karki <39858235+archanakarki@users.noreply.github.com>
2020-02-12 13:31:41 +0200

    Added functionality to create todos item

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -18,6 +18,17 @@ def __repr__(self):
 
 db.create_all()
 
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+  description = request.form.get('description', '')
+  todo = Todo(description=description)
+  db.session.add(todo)
+  db.session.commit()
+  return redirect(url_for('index'))
+
 @app.route('/')
 def index():
     return render_template('index.html', data=Todo.query.all())
+
+
+
