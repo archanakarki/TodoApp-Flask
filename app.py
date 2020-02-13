81ea1924e83b44e0e4956dcf55c008ae9cd834b5
Archana Karki <39858235+archanakarki@users.noreply.github.com>
2020-02-13 06:13:23 +0200

    Update functionality in todo list executed with completed checkboxes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import sys
 
 app = Flask(__name__)
@@ -8,21 +9,27 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://archanakarki@localhost:5432/
 
 db = SQLAlchemy(app)
 
+#Instantiating migrate
+migrate = Migrate(app , db)
+
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
 # Debugging statement
 def __repr__(self):
     return f'<Todo {self.id} {self.description}>'
 
-db.create_all()
+# This db.create_all() command is not required when flask-migrate is run using flask db init
+# db.create_all()
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
     error = False
     body = {}
+    #Implementation of try-except-finally pattern for error handling
     try:
         description = request.get_json()['description']
         todo = Todo(description=description)
@@ -35,15 +42,30 @@ def create_todo():
         print(sys.exc_info())
     finally:
         db.session.close()
-    if not error:
+    if error:
+        abort(400)
+    else:
         return jsonify(body)
+
+# Update todo completed checkbox route
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
 
 
     
-
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.all())
+    return render_template('index.html', data=Todo.query.order_by('id').all())
 
 
 
